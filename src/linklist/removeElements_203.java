869b7ac9a0892331458136f03c54bb3e5dbe4e5d
sdzwyyy <wyzz973@163.com>
2025-02-07 21:17:35 +0800

    链表

---
+++ src/linklist/removeElements_203.java
@@ -0,0 +1,33 @@
+package linklist;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+public class removeElements_203 {
+    public ListNode removeElements(ListNode head, int val) {
+        while (head != null && head.val == val){
+            head = head.next;
+        }
+        if (head == null){
+            return head;
+        }
+        ListNode pre = head;
+        ListNode cur = head.next;
+        while (cur != null){
+            if (cur.val == val){
+                pre.next = cur.next;
+            }else {
+                pre = cur;
+            }
+            cur = cur.next;
+        }
+        return head;
+    }
+}
